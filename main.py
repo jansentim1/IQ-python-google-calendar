f4a62218063f7ea063cc61f8ea9a8413c4fbf784
jansentim1 <twj.jansen@gmail.com>
2022-03-31 09:48:13 -0300

    5 minutes

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
             webhook = "https://hooks.slack.com/services/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"
             payload = {"text": "Google Calendar API not working! CHECK NOW"}
             send_slack_message(webhook, payload)
-        for i in range(2):
-            log.info(f"{2-i} minutes left before next script runs")
-            time.sleep(120)
+        for i in range(5):
+            log.info(f"{5-i} minutes left before next script runs")
+            time.sleep(60)
